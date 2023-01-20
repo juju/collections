4d120dda6b58c7f00efe77abc37ab3590c0979be
Joseph Phillips <joe.phillips@canonical.com>
2023-01-20 12:09:20 +0100

    Adds SliceToMap to the transform package. It can be used to transform a slice to a map with keys derived from the slice members.

---
+++ transform/package_test.go
@@ -0,0 +1,14 @@
+// Copyright 2023 Canonical Ltd.
+// Licensed under the LGPLv3, see LICENCE file for details.
+
+package transform
+
+import (
+	"testing"
+
+	gc "gopkg.in/check.v1"
+)
+
+func TestPackage(t *testing.T) {
+	gc.TestingT(t)
+}
--- transform/slice.go
+++ transform/slice.go
@@ -12,3 +12,15 @@ func Slice[F any, T any](from []F, transform func(F) T) []T {
 	}
 	return to
 }
+
+// SliceToMap transforms a slice of one type to an equal length
+// map with values from the slice, keyed by values indicated by
+// the input transformation function.
+func SliceToMap[F any, K comparable, V any](from []F, transform func(F) (K, V)) map[K]V {
+	to := make(map[K]V, len(from))
+	for _, oneFrom := range from {
+		k, v := transform(oneFrom)
+		to[k] = v
+	}
+	return to
+}
